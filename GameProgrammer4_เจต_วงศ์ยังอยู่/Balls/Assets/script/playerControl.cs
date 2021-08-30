c928451ac8e59988591b3ca5281b03ffe7b95211
JadeWongyoungyoo <nawaf.w1@gmail.com>
2021-08-30 11:24:32 +0700

    DOit

---
+++ GameProgrammer4_เจต_วงศ์ยังอยู่/Balls/Assets/script/playerControl.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    {
+    private Rigidbody rb;
+    public float speed;
+    private int score;
+    public Text scoreText;
+    public Text winText
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetscoreText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("item")) ;
+        {
+            other.gameObject.SetActive(false);
+            score = score + 1;
+            SetscoreText();
+        }
+    }
+    void SetscoreText()
+    {
+        scoreText.Text = "Score:" + score.ToString();
+        if (score >= 11)
+        {
+            winText = "You win";
+        }
+    }
+
+}
